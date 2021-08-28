e794fbb89e45d6bad3560fff1c8f670cc13a3b0a
Alaedeen <alaedeen95@gmail.com>
2021-08-28 19:33:45 +0100

    fix babel config

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,14 +1,14 @@
 import request from 'supertest'
-import { app } from "../src/app.js";
-import {test, jest} from '@jest/globals';
+import { app } from '../src/app.js'
+import { test } from '@jest/globals'
 
-
-jest.useFakeTimers()
-
-test('Signup a new user', async ()=>{
-    await request(app).post('/users').send({
-        name: 'Jon Doe',
-        email: 'jonDoe@example.com',
-        password: 'JonPass99!!'
-    }).expect(201)
-}) 
+test('Signup a new user', async () => {
+  await request(app)
+    .post('/users')
+    .send({
+      name: 'Jon Doe',
+      email: 'jonDoe@example.com',
+      password: 'JonPass99!!',
+    })
+    .expect(201)
+})
